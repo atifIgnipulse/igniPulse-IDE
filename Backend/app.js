117f9917bc6e7c2863441044575745adef59ed0d
atifIgnipulse <atif.ignipulse@gmail.com>
2025-02-17 07:34:05 -0800

    chngd cors

--- Backend/app.js
+++ Backend/app.js
@@ -10,10 +10,16 @@ const mysql = require('mysql2')
 // 1. create hppt server for app
 const server = http.createServer(app);
 // 2. create io for socket
+const allowedOrigins = ["http://51.24.30.180:8080", "http://igniup.com:8080"]
 const io = new Server(server, {
     cors: {
-        origin: "http://51.24.30.180:8080",
-        methods: ["GET", "POST"]
+        origin: (origin, callback)=>{
+            if(!origin || allowedOrigins.includes(origin)){
+                callback(null, true)
+            }else{
+                callback(new Error("Not Allowed by cors"))
+            }
+        }
     }
 });
 
