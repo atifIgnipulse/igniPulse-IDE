e986017c5066549431fb1f3204e16865a4773012
atifIgnipulse <atif.ignipulse@gmail.com>
2025-02-19 02:10:37 -0800

    chnged url

--- Frontend/src/Pages/PythonIDE.jsx
+++ Frontend/src/Pages/PythonIDE.jsx
@@ -60,7 +60,7 @@ function PythonIDE() {
 
   useEffect(() => {
     if (!socket.current) {
-      socket.current = io("http://51.24.30.180:9000");
+      socket.current = io("http://www.igniup.com:9000");
 
       socket.current.on("pyResponse", (message) => {
         // Display normal responses
